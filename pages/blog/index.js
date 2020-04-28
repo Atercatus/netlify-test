f54b566731ad3258e798ebc8c542c12c5aab324a
Atercatus <myleelee2053@naver.com>
2020-04-29 01:12:43 +0900

    test

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -5,13 +5,13 @@ const importBlogPosts = async () => {
   const markdownFiles = require
     .context("../../content/blog", false, /\.md$/)
     .keys()
-    .map((relativePath) => relativePath.subString(2));
+    .map((relativePath) => relativePath.substring(2));
 
   return Promise.all(
     markdownFiles.map(async (path) => {
       const markdown = await import(`../../content/blog/${path}`);
 
-      return { ...markdown, slug: path.subString(0, path.length - 3) };
+      return { ...markdown, slug: path.substring(0, path.length - 3) };
     })
   );
 };
