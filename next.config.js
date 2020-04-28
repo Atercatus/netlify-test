e4fa4c327b14837fb0933c3f0ad91a067dc7ac93
Atercatus <myleelee2053@naver.com>
2020-04-29 00:57:46 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+const fs = require("fs");
+const blogPostsFolder = "./content/blog";
+
+const getPathForPosts = () =>
+  fs.readdirSync(blogPostsFolder).reduce((acc, postName) => {
+    const trimmedName = postName.substring(0, postName.length - 3);
+
+    return Object.assign(acc, {
+      [`/blog/post/${trimmedName}`]: {
+        page: "/blog/post/[slug]",
+        query: {
+          slug: trimmedName,
+        },
+      },
+    });
+  }, {});
+
 module.exports = {
   webpack: (cfg) => {
     cfg.module.rules.push({
@@ -6,5 +23,11 @@ module.exports = {
       options: { mode: ["react-component"] },
     });
     return cfg;
+  },
+  async exportPathMap(defaultPathMap) {
+    return {
+      ...defaultPathMap,
+      ...getPathForPosts(),
+    };
   },
 };
